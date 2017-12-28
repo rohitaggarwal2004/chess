8917dd92d0e55b658ebc1d2cbe62ad087134abf1
Rohit Aggarwal <rohitaggarwal2004@yahoo.com>
2017-12-28 15:12:17 +0530

    Updating code.

--- HTML5Application/public_html/javascript/javascript.js
+++ HTML5Application/public_html/javascript/javascript.js
@@ -1,8 +1,30 @@
-var fName  = prompt("What's your first name");
+// var fName  = prompt("What's your first name?");
 
-var lName  = prompt("What's your last  name");
+// var lName  = prompt("What's your last  name?");
 
-var age  = prompt("What's your age");
+// var age  = prompt("What's your age?");
 
-console.log("Usee name is " +fName + " " + lName);
-console.log( "User age in " + age);
+// console.log("Usee name is " +fName + " " + lName);
+// // console.log( "User age in " + age);
+
+// var num = 10;
+// while(num<=40)
+// {	
+// 	var x = num%2;
+// 	if(x==0)
+// 	{
+// 		console.log(num)
+// 	}
+// 	num++;
+// }
+
+function isEven(num){
+	if(typeof  num=== "number")
+	{
+		if(num%2==0)	
+		{
+			return "true";
+		}
+	}
+	return "false";
+}
