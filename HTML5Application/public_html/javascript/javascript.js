1a4e60ee341ed5102f0309726c302551baf23700
Rohit Aggarwal <rohitaggarwal2004@yahoo.com>
2017-12-28 12:34:14 +0530

    Updating code.

---
+++ HTML5Application/public_html/javascript/javascript.js
@@ -0,0 +1,8 @@
+var fName  = prompt("What's your first name");
+
+var lName  = prompt("What's your last  name");
+
+var age  = prompt("What's your age");
+
+console.log("Usee name is " +fName + " " + lName);
+console.log( "User age in " + age);
