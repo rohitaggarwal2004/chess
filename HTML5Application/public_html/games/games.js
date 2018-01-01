ea65530843d04289a3080dc570a50bf7caac7bfe
Rohit Aggarwal <rohitaggarwal2004@yahoo.com>
2018-01-01 20:56:36 +0530

    Updating code.

--- HTML5Application/public_html/games/games.js
+++ HTML5Application/public_html/games/games.js
@@ -1,3 +1,43 @@
 var easyLevel = "easy"
 var hardLevel = "hard"
 var currentLevel = easyLevel;
+
+
+ document.querySelector("#easy").addEventListener("click",function(){
+ 	currentLevel = easyLevel;
+	var list = document.getElementById("div3");
+	alert(list.length);
+	//list.removeChild(list.firstChild);
+	// alert("1")
+});
+
+
+	document.querySelector("#hard").addEventListener("click",function(){
+	currentLevel = hardLevel;
+ 	var parentDiv = _setDiv("row","div3", document.querySelector("div"));
+ 	 _setDiv("col-lg-3 col-md-3 col-sm-3",null, parentDiv);
+
+ 	var childDiv = _setDiv("col-lg-2 col-md-2 col-sm-2", null, parentDiv);
+ 	_setDiv("rectangle","color1", childDiv);
+ 	 
+ 	 childDiv =  _setDiv("col-lg-2 col-md-2 col-sm-2", null, parentDiv);
+ 	_setDiv("rectangle","color1", childDiv);
+ 	 
+ 	childDiv =  _setDiv("col-lg-2 col-md-2 col-sm-2", null, parentDiv);
+ 	_setDiv("rectangle","color1", childDiv);
+ 	 
+ 	 _setDiv("col-lg-3 col-md-3 col-sm-3",null, parentDiv);
+});	
+
+	function _setDiv(clazz, id,parentNode)
+	{
+		var div = document.createElement("div");                       // Create a <p> element
+		div.setAttribute("class", clazz);
+		if(id!=null)
+		{
+			div.setAttribute("id", id);	
+		}
+		parentNode.appendChild(div);
+
+		return div;
+	}
